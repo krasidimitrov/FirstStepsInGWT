ed0beccf3216bf794cb97edbe441c31d4f13d52a
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-05-28 09:44:58 +0300

    added class for testing the LoginPresenter

---
+++ samplemvp/test/LoginPresenterTest.java
@@ -0,0 +1,11 @@
+import org.jmock.integration.junit4.JMock;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Krasimir Dimitrov (kpackapgo@gmail.com, krasimir.dimitrov@clouway.com)
+ */
+
+
+@RunWith(JMock.class)
+public class LoginPresenterTest {
+}
