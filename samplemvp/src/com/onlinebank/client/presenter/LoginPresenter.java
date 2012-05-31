98a0d71e515faba1a7c16ece231801d81f4dc8db
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-05-31 18:35:09 +0300

    added functionality for clearing the messageLabel

--- samplemvp/src/com/onlinebank/client/presenter/LoginPresenter.java
+++ samplemvp/src/com/onlinebank/client/presenter/LoginPresenter.java
@@ -1,40 +1,67 @@
 package com.onlinebank.client.presenter;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.onlinebank.client.BankServiceAsync;
 import com.onlinebank.client.event.GoToRegistrationButtonClickedEvent;
+import com.onlinebank.client.exception.UserNotRegisteredException;
+import com.onlinebank.client.exception.WrongPasswordException;
+import com.onlinebank.client.model.User;
 import com.onlinebank.client.view.LoginView;
 
 /**
  * @author Krasimir Dimitrov (kpackapgo@gmail.com, krasimir.dimitrov@clouway.com)
  */
-public class LoginPresenter implements Presenter, LoginView.Presenter{
+public class LoginPresenter implements Presenter, LoginView.Presenter {
 
   private final BankServiceAsync rpcService;
   private final EventBus eventBus;
   private final LoginView loginView;
+  private final LoginMessages loginMessages;
 
-  public LoginPresenter(BankServiceAsync rpcService, EventBus eventBus, LoginView loginView) {
+  public LoginPresenter(BankServiceAsync rpcService, EventBus eventBus, LoginView loginView, LoginMessages loginMessages) {
     this.rpcService = rpcService;
     this.eventBus = eventBus;
     this.loginView = loginView;
-    this.loginView.setPresenter(this);
+    this.loginMessages = loginMessages;
   }
 
   @Override
   public void go(HasWidgets container) {
     container.clear();
-    container.add((Widget) loginView);  }
+    loginView.setPresenter(this);
+    container.add((Widget) loginView);
+  }
 
   @Override
   public void onGotToRegistrationButtonClicked() {
+    loginView.setStatusMessage("");
     eventBus.fireEvent(new GoToRegistrationButtonClickedEvent());
   }
 
   @Override
   public void onLoginButtonClicked() {
-    //To change body of implemented methods use File | Settings | File Templates.
+    login(loginView.getUsername(), loginView.getPassword());
+  }
+
+  public void login(String username, String password) {
+    rpcService.login(username, password, new AsyncCallback<User>() {
+      @Override
+      public void onFailure(Throwable caught) {
+        if (caught instanceof UserNotRegisteredException) {
+          loginView.setStatusMessage(loginMessages.getNotRegisteredMessage());
+        }
+        if (caught instanceof WrongPasswordException){
+          loginView.setStatusMessage(loginMessages.getWrongPasswordMessage());
+        }
+      }
+
+      @Override
+      public void onSuccess(User result) {
+        loginView.goToUserPage();
+      }
+    });
   }
 }
