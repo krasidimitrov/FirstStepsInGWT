d6420905c482f9194ae684860ed8f013626dfe22
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-05-31 18:42:06 +0300

    addes some methods without real implementation

--- samplemvp/src/com/onlinebank/client/BankService.java
+++ samplemvp/src/com/onlinebank/client/BankService.java
@@ -2,9 +2,18 @@ package com.onlinebank.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.onlinebank.client.exception.IncorrectDataFormatException;
 import com.onlinebank.client.model.User;
+
+import java.math.BigDecimal;
 
 @RemoteServiceRelativePath("OnlineBankService")
 public interface BankService extends RemoteService {
-  void register(User user);
+  void register(User user) throws IncorrectDataFormatException;
+
+  User login(String username, String password);
+
+  BigDecimal deposit(BigDecimal money);
+
+  BigDecimal withdraw(BigDecimal money);
 }
--- samplemvp/src/com/onlinebank/server/BankServiceImpl.java
+++ samplemvp/src/com/onlinebank/server/BankServiceImpl.java
@@ -2,12 +2,34 @@ package com.onlinebank.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.onlinebank.client.BankService;
+import com.onlinebank.client.exception.IncorrectDataFormatException;
 import com.onlinebank.client.model.User;
+
+import java.math.BigDecimal;
 
 public class BankServiceImpl extends RemoteServiceServlet implements BankService {
 
   @Override
   public void register(User user) {
+    if(user.getUsername().matches("^[A-Za-z0-9]{5,20}$")){
+      return;
+    } else {
+      throw new IncorrectDataFormatException();
+    }
+  }
 
+  @Override
+  public User login(String username, String password) {
+   return null;
+  }
+
+  @Override
+  public BigDecimal deposit(BigDecimal money) {
+    return new BigDecimal(10);
+  }
+
+  @Override
+  public BigDecimal withdraw(BigDecimal money) {
+    return new BigDecimal(5);
   }
 }
