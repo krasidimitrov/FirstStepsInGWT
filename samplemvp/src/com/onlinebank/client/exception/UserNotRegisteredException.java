071e408fecff6b4fa8b625de6f018c93b4b993ab
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-05-31 18:33:53 +0300

    exceptions

---
+++ samplemvp/src/com/onlinebank/client/exception/UserNotRegisteredException.java
@@ -0,0 +1,7 @@
+package com.onlinebank.client.exception;
+
+/**
+ * @author Krasimir Dimitrov (kpackapgo@gmail.com, krasimir.dimitrov@clouway.com)
+ */
+public class UserNotRegisteredException extends RuntimeException {
+}
